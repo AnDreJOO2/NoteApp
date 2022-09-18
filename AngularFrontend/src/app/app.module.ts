f3a729376b55d4476c100b2942670f02c8e1cda4
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-09-18 19:22:30 +0200

    update

---
+++ AngularFrontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NoteService } from './note.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [NoteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
